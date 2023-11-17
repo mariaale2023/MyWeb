1de995fdc0cda4a5da94aa36aa1900e86282e706
Mariaale2023 <123910242+mariaale2023@users.noreply.github.com>
2023-11-17 17:47:18 +1300

    Crete vfile Bird, Sky, and Plane
    Set up Sly 3D model as primitive

---
+++ src/models/Bird.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import birdScene from "../assets/3d/bird.glb";
+
+const Bird = () => {
+  return <mesh></mesh>;
+};
+
+export default Bird;
---
+++ src/models/Plane.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import planeScene from "../assets/3d/plane.glb";
+
+const Plane = () => {
+  return <mesh></mesh>;
+};
+
+export default Plane;
