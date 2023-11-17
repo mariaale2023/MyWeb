3cddc176fb617f11cfd6ddf012fe31486b239f71
Mariaale2023 <123910242+mariaale2023@users.noreply.github.com>
2023-11-17 19:38:26 +1300

    add planeScene and properties for useGLTF Add position, scale and rotation

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
 import planeScene from "../assets/3d/plane.glb";
+import { useGLTF } from "@react-three/drei";
 
 const Plane = () => {
-  return <mesh></mesh>;
+  const { scene, animation } = useGLTF(planeScene);
+
+  return (
+    <mesh position={[0, -1.2, 0]} scale={[2, 2, 2]} rotation={[0, 1.5, 0.1]}>
+      {/* position={[-5, 2, 1]} scale={[0.04, 0.04, 0.04]} */}
+      <primitive object={scene} />
+    </mesh>
+  );
 };
 
 export default Plane;
