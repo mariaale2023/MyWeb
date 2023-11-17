76b755285c699b92dffd9e1f095d5783c40f0d4d
Mariaale2023 <123910242+mariaale2023@users.noreply.github.com>
2023-11-17 17:46:37 +1300

    Add directionLight, ambientLight, hemisphereLight
    to make more bright and center the position of the islan in the centre of the windiow
    Started position is: Door of the house is in front

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,9 @@ import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loder from "../components/Loder";
 import Island from "../models/Island";
+import Sky from "../models/Sky";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
 
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex justify-center items-center">
@@ -12,7 +15,7 @@ import Island from "../models/Island";
 const Home = () => {
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
-    let screenPosition = [0, -6.5, -43];
+    let screenPosition = [0, -6.7, -43];
     let rotation = [0.1, 4.7, 0];
 
     if (window.innerWidth < 768) {
@@ -35,16 +38,24 @@ const Home = () => {
         {/* Suspense from https://react.dev/reference/react/Suspense 
         <Suspense> lets you display a fallback until its children have finished loading.*/}
         <Suspense fallback={<Loder />}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          {/* <pointLight /> */}
+          {/* <spotLight /> */}
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundcolor="#000000"
+            intensity={0.5}
+          />
+
+          <Bird />
+          <Sky />
           <Island
             scale={islandScale}
             position={islandPosition}
             rotation={islandRotation}
           />
+          <Plane />
         </Suspense>
       </Canvas>
     </section>
