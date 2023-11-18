008590d32651d398c368c16597e3f4d8fa84e045
Mariaale2023 <123910242+mariaale2023@users.noreply.github.com>
2023-11-19 11:09:12 +1300

    add handlepointer up, dons, and move, releteed with X coordinates and window

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -17,7 +17,52 @@ import { a } from "@react-spring/three";
 import islandScene from "../assets/3d/island.glb";
 
 const Island = ({ isRotating, setIsRotating, ...props }) => {
+  // seRef is a React Hook that provides a way to create a mutable object that persists throughout the lifecycle of a component. manipulate properties of the 3D object represented by the Island component.
   const islandRef = useRef();
+
+  const { gl, viewport } = useThree();
+
+  const lastX = useRef(0);
+  const rotationSpeed = useRef(0);
+  // how much is continue moving after the start rotationg the island
+  const dampingFactor = 0.95;
+
+  const handlePointerDown = (e) => {
+    e.stopPropagantion();
+    e.preventDefault();
+    setIsRotating(true);
+
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+
+    lastX.current = clientX;
+  };
+  const handlePointerUp = (e) => {
+    e.stopPropagantion();
+    e.preventDefault();
+    setIsRotating(false);
+
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+
+    const delta = (clientX - lastX.current) / viewport.width;
+
+    islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+
+    lastX.current = clientX;
+
+    rotationSpeed.current = delta * 0.01 * Math.PI;
+  };
+  const handlePointerMove = (e) => {
+    e.stopPropagantion();
+    e.preventDefault();
+
+    if (isRotating) {
+      handlePointerUp(e);
+    }
+  };
+
+  useEffect(() => {
+    document.rom;
+  }, [gl, handlePointerDown, handlePointerMove, handlePointerUp]);
   // useGLTF is used to load the 3D model from the specified GLB file
   const { nodes, materials } = useGLTF(islandScene);
   return (
