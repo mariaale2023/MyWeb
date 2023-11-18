6cbc7656efe71c1bd4b311c0945f376c7a7c1148
Mariaale2023 <123910242+mariaale2023@users.noreply.github.com>
2023-11-18 18:00:08 +1300

    Add rotation, scale, postion to Plane from Home and pass it has propt on Plane file

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -3,12 +3,18 @@ import React from "react";
 import planeScene from "../assets/3d/plane.glb";
 import { useGLTF } from "@react-three/drei";
 
-const Plane = () => {
+const Plane = (rotation, planePosition, planeScale, isRotating) => {
   const { scene, animation } = useGLTF(planeScene);
 
   return (
-    <mesh position={[0, -1.2, 0]} scale={[2, 2, 2]} rotation={[0, 1.5, 0.1]}>
-      {/* position={[-5, 2, 1]} scale={[0.04, 0.04, 0.04]} */}
+    // <mesh position={[0, -1.2, 0]} scale={[2, 2, 2]} rotation={[0, 1.5, 0.1]}> */}
+    // //  position={[-5, 2, 1]} scale={[0.04, 0.04, 0.04]} */}
+    <mesh
+      rotation={[0, 20, 0]}
+      planePosition={planePosition}
+      planeScale={planeScale}
+      isRotating={isRotating}
+    >
       <primitive object={scene} />
     </mesh>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { useState, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loder from "../components/Loder";
 import Island from "../models/Island";
@@ -13,6 +13,9 @@ import Plane from "../models/Plane";
 }
 
 const Home = () => {
+  //  For the rotation of the Island and the Plane
+  const [isRotating, setIsRotating] = useState(false);
+
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [0, -6.7, -43];
@@ -25,14 +28,30 @@ const Home = () => {
     }
     return [screenScale, screenPosition, rotation];
   };
+  const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition;
+
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+    return [screenScale, screenPosition];
+  };
 
   const [islandScale, islandPosition, islandRotation] =
     adjustIslandForScreenSize();
 
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
       <Canvas
-        className="w-full h-screen bg-transparent"
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
         camera={{ near: 0.1, far: 1000 }}
       >
         {/* Suspense from https://react.dev/reference/react/Suspense 
@@ -54,8 +73,17 @@ const Home = () => {
             scale={islandScale}
             position={islandPosition}
             rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+
+            // isSetRotating={isSetRotating}
           />
-          <Plane />
+          <Plane
+            planePosition={planePosition}
+            planeScale={planeScale}
+            isRotating={isRotating}
+            rotation={[0, 20, 0]}
+          />
         </Suspense>
       </Canvas>
     </section>
