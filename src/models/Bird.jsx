58dd23744c191b204de6de9f8e3d25073f547c5f
Mariaale2023 <123910242+mariaale2023@users.noreply.github.com>
2023-11-22 13:04:05 +1300

    Simulate fly of bird Use clock and camera promps and useFrame Utilise Math.sin() to simulate a wave Use if condition to move the bird releated with the camera position

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,6 +1,7 @@
 import { useRef, useEffect } from "react";
 
 import { useAnimations, useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
 import birdScene from "../assets/3d/bird.glb";
 
@@ -13,6 +14,31 @@ const Bird = () => {
   useEffect(() => {
     actions["Take 001"].play();
   }, []);
+
+  useFrame(({ clock, camera }) => {
+    //Update the Y position to simulate bird-like motion using a sine wave
+    birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
+
+    if (birdRef.current.position.x > camera.position.x + 10) {
+      //change direction to backword and rotate the bird 180 degrees onm the y-axis
+      birdRef.current.rotation.y = Math.PI;
+    } else if (birdRef.current.position.x < camera.position.x - 10) {
+      // change direction to forward and reset the bird's rotation
+      birdRef.current.rotation.y = 0;
+    }
+
+    // Update X and Z position base on the direction
+    if (birdRef.current.rotation.y === 0) {
+      // Moving forward
+      birdRef.current.position.x += 0.01;
+      birdRef.current.position.z -= 0.001;
+    } else {
+      //moving backward
+      birdRef.current.position.x -= 0.01;
+      birdRef.current.position.z += 0.001;
+    }
+  });
+
   return (
     <mesh position={[-5, 2, 1]} scale={[0.005, 0.005, 0.005]} ref={birdRef}>
       <primitive object={scene} />
