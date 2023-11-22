ae8979d2d6a437c8bb79881ce6b6b217a921da48
Mariaale2023 <123910242+mariaale2023@users.noreply.github.com>
2023-11-22 15:23:47 +1300

    Set up input in contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  contact[(form, setForm)] = useState({ name: "", email: "", message: "" });
+
+  const handleChange = () => {};
+  const handleFocus = () => {};
+  const handleBlur = () => {};
+
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container bg-white ">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in Touch</h1>
+
+        <form className="w-full flex flex-col gap-7 mt-14">
+          <label className="text-black-500 font-semibold">
+            Name
+            <imput
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Maria"
+              requiered
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
