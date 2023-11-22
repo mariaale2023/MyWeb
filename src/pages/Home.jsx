70d5bdd7b65d691dbf94fe01ecf1834bedae933f
Mariaale2023 <123910242+mariaale2023@users.noreply.github.com>
2023-11-22 13:05:19 +1300

    create HomeIfo component make areused react component to setup the Pop wellcom message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,7 @@ import Island from "../models/Island";
 import Sky from "../models/Sky";
 import Bird from "../models/Bird";
 import Plane from "../models/Plane";
-
-{
-  /* <div className="absolute top-28 left-0 right-0 z-10 flex justify-center items-center">
-        POP UP
-      </div> */
-}
+import HomeInfo from "../components/HomeInfo";
 
 const Home = () => {
   //  For the rotation of the Island and the Plane
@@ -58,6 +53,9 @@ const Home = () => {
 
   return (
     <section className="w-full h-screen relative">
+      <div className="absolute top-28 left-0 right-0 z-10 flex justify-center items-center">
+        {currentStage && <HomeInfo currentStage={currentStage} />}
+      </div>
       <Canvas
         className={`w-full h-screen bg-transparent ${
           isRotating ? "cursor-grabbing" : "cursor-grab"
