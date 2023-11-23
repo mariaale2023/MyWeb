2a115038fb76beb17479d71a0fb1136478a0ac7c
Mariaale2023 <123910242+mariaale2023@users.noreply.github.com>
2023-11-23 14:54:56 +1300

    Add Skills and Experience import Object with list  Tools-language icons create an object with list of work experince create an object with list of projects

---
+++ src/constants/index.js
@@ -0,0 +1,247 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+  car,
+  contact,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  pricewise,
+  react,
+  redux,
+  sass,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript,
+} from "../assets/icons";
+
+export const skills = [
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: express,
+    name: "Express",
+    type: "Backend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: motion,
+    name: "Motion",
+    type: "Animation",
+  },
+  {
+    imageUrl: mui,
+    name: "Material-UI",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Project Coordinator (Network PILA project)  ",
+    company_name: "Intellectual Property Department at University of Chile ",
+    icon: bank,
+    iconBg: "#accbe1",
+    date: "2010 - 2011",
+    points: [
+      "Established collaborative network among Latin American universities and IP specialists, conducted national benchmarking, managed budget, and formed partnerships for impactful seminars with 14 Chilean universities and government entities.",
+      "Positioned PILA project through strategic planning and dissemination activities, organizing 4 successful seminars",
+    ],
+  },
+  {
+    title: "Project Coordinator ",
+    company_name: "National Bank of Chile ",
+    icon: bank,
+    iconBg: "#accbe1",
+    date: "2011 - 2013",
+    points: [
+      "Lead limited scope projects using  Agile principles with strict timeframes and budget, particularly in commercial platform triggered by legislation changes or commercial requests like modifications and data capture requirements.",
+    ],
+  },
+  {
+    title: "B2B Sales Digital Services",
+    company_name: "Telefonica Company",
+    icon: telefonica,
+    iconBg: "#fbc3bc",
+    date: "2013 - 2016",
+    points: [
+      "Collaborated with technology partners to promote digital services, trained sales forces, and collaborated with technology partners and tech teams on custom solutions in Cloud Computing, InfoSec, CCTV, SaaS, and IT Financial Services.",
+    ],
+  },
+  {
+    title: "Senior IT Sales",
+    company_name: "Telefonica Company",
+    icon: telefonica,
+    iconBg: "#b7e4c7",
+    date: "2016 -  2018",
+    points: [
+      "Drove sales growth and transformed retail operations in Latin America through customized IT services and collaborations with top brands like SAP, Cisco, HP, Microsoft, Huawei, and other.",
+    ],
+  },
+  {
+    title: "Shopify Developer and MKT Lead",
+    company_name: "NZBike.co.nz",
+    icon: shopify,
+    iconBg: "#a2d2ff",
+    date: "Ago 2023 - Present",
+    points: [
+      "Design, Strategic planning and implementation eCommerce integrated with 4 supplier for one of the brand of the business.",
+      "Ensure smooth connection with the Shopify Apps, Marketing Tools, and Platform APIs.",
+      "Modify and update current sites to incorporate new features and enhance UI/UX design",
+      "Developing, monitoring, writing and updating content contained on eCommerce websites and social media.",
+    ],
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/mariaale2023",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/maria-vasquez-nz/",
+  },
+];
+
+export const projects = [
+  {
+    iconUrl: shopify,
+    theme: "btn-back-red",
+    name: "NZBike Store",
+    description:
+      "Developed a shopify web store that is an extension of the renowned New Zealand Motorcycle Rentals and Tours, we bring you a comprehensive range of motorcycle gear, accessories, parts, clothing, and more.",
+    link: "https://nzbike.co.nz/",
+  },
+  {
+    iconUrl: threads,
+    theme: "btn-back-green",
+    name: "Shortflix -Full Stack Netflix Clone",
+    description:
+      "The Shortflix Blog is a web application that allows users to share and discover information about their favorite movies. Users can create accounts, browse through a collection of movies, read and write reviews, and interact with other movie enthusiasts through comments and likes.",
+    link: "https://github.com/mariaale2023/Shortflix",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-yellow",
+    name: "Touch the Grass Store",
+    description:
+      "Touch the Grass is an innovative store that seamlessly blends fashion and home décor with the timeless beauty of grass. Our unique approach to retailing offers a curated collection of clothing and furniture, all adorned with high-quality, sustainable grass materials.",
+    link: "https://touchthegrass.onrender.com/",
+  },
+  {
+    iconUrl: car,
+    theme: "btn-back-blue",
+    name: "Cat-ia Chatbox",
+    description:
+      "Designed a Cat-chatbot that help you to alleviate frustration and wasted time searching your misplaced items in your home. This will provide you with step-by-step directions on where to locate the item within the house..",
+    link: "https://chatbot-detective-cat.onrender.com/",
+  },
+  {
+    iconUrl: snapgram,
+    theme: "btn-back-pink",
+    name: "Guess The South America Country",
+    description:
+      "Guess The South America Country is a simple web project that challenges your knowledge about the 12 countries in South America. In this game, you will have three chances to guess the correct country. The project includes three files: index.html, style.css, and script.js. You can check out the live demo of the project.",
+    link: "https://guess-southamerica-country.netlify.app/",
+  },
+  {
+    iconUrl: estate,
+    theme: "btn-back-black",
+    name: "Ricky Morty App",
+    description:
+      "The Ricky Morty App is a web application that allows users to explore characters from the popular animated TV show Rick and Morty. Users can view details about each character, search for specific characters, and access a shop for Ricky Morty merchandise. This App is connected with Ricky and Morty API and Higrogen Shopify framework.",
+    link: "https://github.com/mariaale2023/Ricky-Morty-App",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-yellow",
+    name: "TrustBankApp",
+    description:
+      "Built a Bank application enables users to deposit and request funds across a range of predetermined accounts. It leverages JavaScript principles such as manipulating the Document Object Model (DOM), utilizing functions, and primarily employing arrays and array methods.",
+    link: "https://github.com/mariaale2023/TrustBankApp",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-yellow",
+    name: "Google Clone",
+    description:
+      "Built Google is a clone of Google Search Engine with HTML, CSS and JS.",
+    link: "https://google-clone-mariavasquez.netlify.app/#",
+  },
+];
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { skills } from "../constants";
 
 const About = () => {
   return (
@@ -33,9 +34,34 @@ const About = () => {
           Let's collaborate and bring your projects to life!
         </p>
       </div>
-      <div className="py-10 flex flex-col">
+      <div className=" my-skills py-10 flex flex-col">
         <h2 className="subhead-text">My Skills</h2>
-        <div className="mt-16 flex flex-wrap gap-12"></div>
+        <div className="mt-16 flex flex-wrap gap-12">
+          {skills.map((skill) => (
+            <div className="block-container w-20 h-20">
+              <div className="btn-back rounded-xl" />
+              <div className=" btn-front rounded-xl flex justify-center items-center">
+                <img
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className="w-1/2 h-1/2 object-contain "
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="work-experience py-16">
+        <h3>Work Experience</h3>
+        <div className="mt-5 flex flex-col gap-3 text-slate-500">
+          <p>
+            As a passionate Front-end Developer originally from Chile and
+            currently based in the beautiful landscapes of New Zealand, I bring
+            over 8 years of expertise in business and project management to the
+            world of web development.
+          </p>
+        </div>
       </div>
     </section>
   );
